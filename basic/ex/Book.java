b3075880e1ff076e847a8053112068de8e484ecb
Zeroback101 <tmddus7776@naver.com>
2024-11-19 17:14:44 +0900

    Create Book.java

---
+++ basic/ex/Book.java
@@ -0,0 +1,20 @@
+package basic.ex;
+
+public class Book {
+  String title;
+  String author;
+  int page;
+
+  Book() {
+    
+  }
+  Book(title, author) {
+    this(title, author);
+  }
+  Book(title, author, page) {
+    this(title, author, page);
+  }
+  static void displayinfo() {
+    System.out.println("제목: " + title + "저자: " + author + "페이지 수: " + page);
+  }
+}
