d71f7ebd0efbf7f886061d0280318b2b09e4c95a
Zeroback101 <tmddus7776@naver.com>
2024-11-19 15:48:10 +0900

    Create MusicPlayerOOPMain1.java

---
+++ basic/02oop/MusicPlayerOOPMain1.java
@@ -0,0 +1,13 @@
+package basic.02oop;
+
+public class MusicPlayerOOPMain3 {
+  public static void main(String[] args) {
+    MusicPlayerDataOOP data = new MusicPlayerDataOOP();
+
+    data.on();
+    data.valumeUp();
+    data.valumeDown();
+    data.showStatus();
+    data.off();
+  }
+}
