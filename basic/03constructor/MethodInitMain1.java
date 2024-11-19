947b094b9af84a623c329d207591b424359b39cf
Zeroback101 <tmddus7776@naver.com>
2024-11-19 16:49:34 +0900

    Update MethodInitMain1.java

--- basic/03constructor/MethodInitMain1.java
+++ basic/03constructor/MethodInitMain1.java
@@ -1,5 +1,8 @@
 package basic.03constructor;
 
 public class MethodInitMain1 {
-  
+  public static void main(String[] args) {
+    MemberInit member = new MemberInit();
+    member.initMember("철수", 20, 3);
+  }
 }
