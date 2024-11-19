c910c613b1ee5853a233459c27c6fecc975d90c5
Zeroback101 <tmddus7776@naver.com>
2024-11-19 15:41:48 +0900

    Update ValueObject.java

--- basic/02oop/ValueObject.java
+++ basic/02oop/ValueObject.java
@@ -3,8 +3,8 @@ package basic.02oop;
 public class ValueObject {
   int value;
 
-  static void add(ValueObject object) {
-    object.value++;
-    System.out.println("숫자 증가 value = " + object.value);
+  static void add() {
+    value++;
+    System.out.println("숫자 증가 value = " + value);
   }
 }
