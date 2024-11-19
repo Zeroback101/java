d92687dafa9f4cd96a06a4abe28a0761a1216bc2
Zeroback101 <tmddus7776@naver.com>
2024-11-19 16:58:50 +0900

    Create MemberConstructorMain1.java

---
+++ basic/03constructor/MemberConstructorMain1.java
@@ -0,0 +1,8 @@
+package basic.03constructor;
+
+public class MemberConstructorMain1 {
+  public static void main(String[] args) {
+    MemberConstructor member1 = new MemberConstructor("철수", 20, 3);
+    MemberConstructor member2 = new MemberConstructor("영희", 22, 4);
+  }
+}
