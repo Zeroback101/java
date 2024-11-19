427cb2156ed9af1c8cf0f2ee5334c2971966e043
Zeroback101 <tmddus7776@naver.com>
2024-11-19 16:59:10 +0900

    Update and rename MethodInitMain1.java to MemberInitMain1.java

--- basic/03constructor/MemberInitMain1.java
+++ basic/03constructor/MemberInitMain1.java
@@ -1,6 +1,6 @@
 package basic.03constructor;
 
-public class MethodInitMain1 {
+public class MemberInitMain1 {
   public static void main(String[] args) {
     MemberInit member = new MemberInit();
     member.initMember("철수", 20, 3);
