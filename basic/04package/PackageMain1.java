d7d5384ec1223179e0f961e9093f2b89b1543e67
Zeroback101 <tmddus7776@naver.com>
2024-11-20 10:09:53 +0900

    Create PackageMain1.java

---
+++ basic/04package/PackageMain1.java
@@ -0,0 +1,8 @@
+package basic.04package
+
+public class PackageMain1 {
+  public static void main(String[] args) {
+    Data data = new Data();
+    basic.pack.User = new basic.pack.User(); // 다른 패키지에 있는 경우
+  }
+}
