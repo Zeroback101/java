9342117711a805c974047075227542eeafa269c0
Zeroback101 <tmddus7776@naver.com>
2024-11-20 10:15:09 +0900

    Update PackageMain1.java

--- basic/04package/PackageMain1.java
+++ basic/04package/PackageMain1.java
@@ -3,6 +3,6 @@ package basic.04package
 public class PackageMain1 {
   public static void main(String[] args) {
     Data data = new Data();
-    basic.pack.User = new basic.pack.User(); // 다른 패키지에 있는 경우
+    basic.04package.a.User user = new basic.04package.a.User(); // 다른 패키지에 있는 경우
   }
 }
