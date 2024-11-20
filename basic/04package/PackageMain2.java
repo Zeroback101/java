2a1a394cb1413bcb873339a60e8e86502d55a185
Zeroback101 <tmddus7776@naver.com>
2024-11-20 10:18:52 +0900

    Update PackageMain2.java

--- basic/04package/PackageMain2.java
+++ basic/04package/PackageMain2.java
@@ -1,7 +1,7 @@
 package basic.04package;
 
-import basic.pack.User; // import: 다른 패키지에 있는 경우
-import basic.pack.*
+import basic.04package.a.User; // import: 다른 패키지에 있는 경우
+import basic.04package.a.* // 해당 패키지의 모든 파일을 임포트
   
 public class PackageMain2 {
   public static void main(String[] args) {
