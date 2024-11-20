76c84113361f63673964ec343011e1f641fecc6e
Zeroback101 <tmddus7776@naver.com>
2024-11-20 10:14:21 +0900

    Create PackageMain2.java

---
+++ basic/04package/PackageMain2.java
@@ -0,0 +1,11 @@
+package basic.04package;
+
+import basic.pack.User; // import: 다른 패키지에 있는 경우
+import basic.pack.*
+  
+public class PackageMain2 {
+  public static void main(String[] args) {
+    Data data = new Data();
+    User user = new User(); 
+  }
+}
