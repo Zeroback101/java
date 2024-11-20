05df7f127bdd0afe9326081772852121c575b104
Zeroback101 <tmddus7776@naver.com>
2024-11-20 15:41:51 +0900

    Create PublicClass.java

---
+++ basic/05access/PublicClass.java
@@ -0,0 +1,12 @@
+package basic.05access;
+
+public class PublicClass {
+  public static void main(String[] args) {
+    PublicClass publicclass = new PublicClass();
+    DefaultClass defualtClass = new DefaultClass();
+  }
+}
+
+class DefaultClass {
+  
+}
