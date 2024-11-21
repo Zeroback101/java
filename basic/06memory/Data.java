083123921e87bc47a8dcdcf30bccf73e05e09b6e
Zeroback101 <tmddus7776@naver.com>
2024-11-21 17:23:02 +0900

    Create Data.java

---
+++ basic/06memory/Data.java
@@ -0,0 +1,13 @@
+package basic.06memory;
+
+public class Data {
+  private int value;
+
+  public Data(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+}
