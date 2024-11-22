7cdc7557af25b3906f9c95edf0b9b7b598f436b8
Zeroback101 <tmddus7776@naver.com>
2024-11-22 20:57:16 +0900

    Create FieldInit.java

---
+++ basic/07final/FieldInit.java
@@ -0,0 +1,14 @@
+package basic.07final;
+
+public class FieldInit {
+    // 자바에서 static final은 상수이므로 변수명을 대문자로 작성한다.
+    static final int CONST_VALUE = 10;
+    final int value = 10; // 초기값이 이미 할당되면 생성자를 통해서 변경하지못함 -> 생성자로 변경 불가
+    // final int는 값을 바꾸지 못하는 10의 값이 이미 할당되어버림, 모든 인스턴스가 같은 값을 사용하므로 인스턴스 변수로 사용하게 되면 인스턴스에서 같은 값을 사용하면서 메모리 낭비를 하게됨, 또한 중복 코드를 양성함
+    // 그렇기 떄문에 static final int로 사용하게되면 메서드 영역에 생성되므로 메모리 최적화가 되며 인스턴스생성 할 때마다 메모리 낭비를 하지 않아도됨
+    // 이러한 이유 때문에 final 초기화를 하는경우 static을 사용하는 것이 효과적임
+
+    public FieldInit() {
+        this.value = 20; // 재할당 불가: error
+    }
+}
