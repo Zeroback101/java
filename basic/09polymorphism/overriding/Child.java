22334a3d597b51369b59401148614f6d8cf70957
Zeroback101 <tmddus7776@naver.com>
2024-11-23 17:07:12 +0900

    Create Child.java

---
+++ basic/09polymorphism/overriding/Child.java
@@ -0,0 +1,10 @@
+package basic.09polymorphism.overriding;
+
+public class Child extends Parent {
+    public String value = "child";
+
+    @Override
+    public void method() {
+        System.out.println("Child.method");
+    }
+}
