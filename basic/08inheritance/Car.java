899d91b4d0cda8173407afda9208818f65b39b4d
Zeroback101 <tmddus7776@naver.com>
2024-11-23 14:39:56 +0900

    Update Car.java

--- basic/08inheritance/Car.java
+++ basic/08inheritance/Car.java
@@ -4,4 +4,9 @@ public class Car {
     public void move() {
         System.out.println("move");
     }
+
+    // 기능 추가 -> 부모에 추가했으므로 자식에게 전달된다.
+    public void openDoor() {
+        System.out.println("open door");
+    }
 }
