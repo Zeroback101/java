bd5c509711799e78ee83a3dc4abfe6933d5e74ed
Zeroback101 <tmddus7776@naver.com>
2024-11-23 15:51:21 +0900

    Update Movie.java

--- basic/ex/08inheritance/Movie.java
+++ basic/ex/08inheritance/Movie.java
@@ -5,9 +5,15 @@ public class Movie extends Item{
   private String director;
   private String actor;
 
-  public Book(String name, int price, String director, String actor) {
+  public Movie(String name, int price, String director, String actor) {
     super(name, price);
     this.director = director;
     this.actor = actor;
+  }
+
+  @Override
+  public void print() {
+    System.out.println("이름: " + name + "가격: " + price);
+    System.out.println(" - 감독: " + director + "배우: " + actor);
   }
 }
