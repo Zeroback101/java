54a3822849f6930c2a16af803d6e518003203085
Zeroback101 <tmddus7776@naver.com>
2024-11-23 15:51:51 +0900

    Update Album.java

--- basic/ex/08inheritance/Album.java
+++ basic/ex/08inheritance/Album.java
@@ -8,4 +8,10 @@ public class Album extends Item{
     super(name, price);
     this.artist = artist;
   }
+
+  @Override
+  public void print() {
+    System.out.println("이름: " + name + "가격: " + price);
+    System.out.println(" - 아티스트: " + artist);
+  }
 }
