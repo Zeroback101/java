de220ef2202da1ff91d8ef3c7fe641b5dd938dc4
Zeroback101 <tmddus7776@naver.com>
2024-11-23 16:54:05 +0900

    Create Parent.java

---
+++ basic/09polymorphism/overriding/Parent.java
@@ -0,0 +1,10 @@
+package basic.09polymorphism.overriding;
+
+public class Parent {
+
+    public String value = "Parent";
+
+    public void method() {
+        System.out.println("Parent.method");
+    }
+}
