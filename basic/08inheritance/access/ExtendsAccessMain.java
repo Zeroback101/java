5fd8853c3caf33328e1f721354215ccf5dea463c
Zeroback101 <tmddus7776@naver.com>
2024-11-23 15:19:27 +0900

    Create ExtendsAccessMain.java

---
+++ basic/08inheritance/access/ExtendsAccessMain.java
@@ -0,0 +1,11 @@
+package basic.08inheritance.access;
+
+import basic.08inheritance.access.child.Child;
+
+public class ExtendsAccessMain {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
