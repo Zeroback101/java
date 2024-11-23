bd4f0c1975a7f171e1a58bca0003f51c1805e30a
Zeroback101 <tmddus7776@naver.com>
2024-11-23 15:18:31 +0900

    Create Child.java

---
+++ basic/08inheritance/access/child/Child.java
@@ -0,0 +1,20 @@
+basic.08inheritance.access.child;
+
+import basic.08inheritance.access.parent.Parent;
+
+public class Child extends Parent {
+
+    public void call() {
+        publicValue = 1;
+        protectedValue = 1; // 상속관계 or 같은 패키지
+        defaultValue = 1; // 다른 패키지 접근 불가, error
+        privateValue = 1; // 접근 불가, error
+
+        publicMethod();
+        protectedMethod();
+        defaultMethod(); // error
+        privateMethod(); // error
+
+        printParent(); // 부모에서 default, private 메서드에 접근하는 것이므로 호출 가능
+    }
+}
