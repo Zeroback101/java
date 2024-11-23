444a9d1eb0357b372a1d787254820c2b1cdef71b
Zeroback101 <tmddus7776@naver.com>
2024-11-23 15:16:25 +0900

    Update ElectricCar.java

--- basic/08inheritance/ElectricCar.java
+++ basic/08inheritance/ElectricCar.java
@@ -2,6 +2,11 @@ package basic.08inheritance;
 
 public class ElectricCar extends Car { // ElectricCar -> Car 상속
 
+    @Override 
+    public void move() { // 오버라이딩 = 부모의 기능을 자식이 새로 재정의하는 것
+        System.out.println("fast move");
+    }
+
     public void charge() {
         System.out.println("charge");
     }
