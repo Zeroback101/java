f87ceec31037491f43837a457d41da1898c81d41
Zeroback101 <tmddus7776@naver.com>
2024-11-23 14:40:07 +0900

    Update CarMain.java

--- basic/08inheritance/CarMain.java
+++ basic/08inheritance/CarMain.java
@@ -1,14 +1,21 @@
 package basic.08inheritance;
 
 public class CarMain {
- 
+
     public static void main(String[] args) {
-        ElectricCar electricCar = new ElectricCar(); 
-        electricCar.move(); // 부모 공간에 가서 찾는다.
-        electricCar.charge(); // 자식 공간에 가서 찾는다.
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move(); // 부모 기능 -> 부모에게서 찾음
+        electricCar.charge(); // 자식 기능 -> 자식에게서 찾음
+        electricCar.openDoor(); // 부모 기능
 
         GasCar gasCar = new GasCar();
         gasCar.move();
         gasCar.fillUp();
+        gasCar.openDoor();
+
+        HydrogenCar hydrogenCar = new HydrogenCar();
+        hydrogenCar.move();
+        hydrogenCar.fillHydrogen();
+        hydrogenCar.openDoor();
     }
 }
