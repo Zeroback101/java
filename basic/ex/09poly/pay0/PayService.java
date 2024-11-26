6a42afb4532fb734e0e54bf2e18c35a4c70bce98
Zeroback101 <tmddus7776@naver.com>
2024-11-26 17:34:07 +0900

    Update PayService.java

--- basic/ex/09poly/pay0/PayService.java
+++ basic/ex/09poly/pay0/PayService.java
@@ -1,6 +1,24 @@
 package basic.ex.09poly.pay0;
 
-public abstract class PayService {
+public class PayService {
   
-  public abstract void processPay(String payOpt, int amount);
+  public void processPay(String payOpt, int amount) {
+    System.out.println("결제를 시작합니다: option=" + payOpt + " amount=" + amount);
+    if(payOpt.equals("kakao")) {
+      KakaoPay kakaoPay = new KakaoPay();
+      result = kakaoPay.pay(amount);
+    } else if(payOpt.equals("naver")) {
+      NaverPay naverPay = new NaverPay();
+      result = naverPay.pay(amount);
+    } else {
+      System.out.println("결제 수단이 없습니다.");
+      result = false;
+    }
+
+    if(result) {
+      System.out.println("결제가 성공했습니다.");
+    } else {
+      System.out.println("결제가 실패했습니다.");
+    }
+  }
 }
