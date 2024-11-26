3023a93a534c9f83b05fd87626f980a87f2768a4
Zeroback101 <tmddus7776@naver.com>
2024-11-26 16:26:38 +0900

    Update Bird.java

--- basic/10polymorphism2/Bird.java
+++ basic/10polymorphism2/Bird.java
@@ -2,12 +2,12 @@ package basic.10polymorphism2;
 
 public class Bird extends AbstractAnimal implements Fly { // extends는 하나만 가능하기 때문에 먼저해야함
     @Override
-    public void sound() {
+    public void sound() { // 클래스 상속
         System.out.println("짹짹");
     }
 
     @Override
-    public void fly() {
+    public void fly() { // 인터페이스 구현
         System.out.println("Bird.fly");
     }
 }
