3f38af4b83bb14e5b05e722eb2069640d59716ca
Zeroback101 <tmddus7776@naver.com>
2024-11-26 17:54:50 +0900

    Update NaverPay.java

--- basic/ex/09poly/pay1/NaverPay.java
+++ basic/ex/09poly/pay1/NaverPay.java
@@ -1,9 +1,10 @@
-package basic.ex.09poly.pay0;
+package basic.ex.09poly.pay1;
 
-public class NaverPay implements PayCategory {
+public class NaverPay implements Pay {
   @Override
-  public void pay(int amount) {
+  public boolean pay(int amount) {
     System.out.println("네이버페이 시스템과 연결합니다.");
     System.out.println(amount + "원 결제를 시도합니다.");
+    return true;
   }
 }
