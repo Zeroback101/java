c3169be79c9b44b000aa6d55999d6b86bff0ca2a
Zeroback101 <tmddus7776@naver.com>
2024-11-26 17:54:29 +0900

    Update KakaoPay.java

--- basic/ex/09poly/pay1/KakaoPay.java
+++ basic/ex/09poly/pay1/KakaoPay.java
@@ -1,9 +1,10 @@
 package basic.ex.09poly.pay0;
 
-public class KakaoPay implements PayCategory {
+public class KakaoPay implements Pay {
   @Override
-  public void pay(int amount) {
+  public boolean pay(int amount) {
     System.out.println("카카오페이 시스템과 연결합니다.");
     System.out.println(amount + "원 결제를 시도합니다.");
+    return true;
   }
 }
